82cb7a99e64e866a9a7c996abc9077e18a3ff26a
david710707@gmail.com <david710707@gmail.com>
2017-07-19 10:34:08 +0800

    commit

--- modules/luci-mod-admin-full/luasrc/model/cbi/sdc_system/clock.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/sdc_system/clock.lua
@@ -25,6 +25,102 @@ s.addremove = false
 
 o = s:option(Value, "ntpserver1", translate("Ntp Server 1"))
 o.datatype = "string"
+o.rmempty = false
+o.optional = false
+--o.default = "" 
+
+function o.parse(self, section, novld)
+	local fvalue = self:formvalue(section)
+	local fexist = ( fvalue and (#fvalue > 0) )	-- not "nil" and "not empty"
+	local cvalue = self:cfgvalue(section)
+	local rm_opt = ( self.rmempty or self.optional )
+	local eq_cfg					-- flag: equal cfgvalue
+
+	-- If favlue and cvalue are both tables and have the same content
+	-- make them identical
+	if type(fvalue) == "table" and type(cvalue) == "table" then
+		eq_cfg = (#fvalue == #cvalue)
+		if eq_cfg then
+			for i=1, #fvalue do
+				if cvalue[i] ~= fvalue[i] then
+					eq_cfg = false
+				end
+			end
+		end
+		if eq_cfg then
+			fvalue = cvalue
+		end
+	end
+
+	-- removed parameter "section" from function call because used/accepted nowhere
+	-- also removed call to function "transfer"
+	local vvalue, errtxt = self:validate(fvalue)
+
+	-- error handling; validate return "nil"
+	if not vvalue then
+		if novld then 		-- and "novld" set
+			return		-- then exit without raising an error
+		end
+
+		if fexist then		-- and there is a formvalue
+			self:add_error(section, "invalid", errtxt or self.title .. ": invalid")
+			return		-- so data are invalid
+		elseif not rm_opt then	-- and empty formvalue but NOT (rmempty or optional) set
+			self:add_error(section, "missing", errtxt or self.title .. ": missing")
+			return		-- so data is missing
+		elseif errtxt then
+			self:add_error(section, "invalid", errtxt)
+			return
+		end
+--		error  ("\n option: " .. self.option ..
+--			"\n fvalue: " .. tostring(fvalue) ..
+--			"\n fexist: " .. tostring(fexist) ..
+--			"\n cvalue: " .. tostring(cvalue) ..
+--			"\n vvalue: " .. tostring(vvalue) ..
+--			"\n vexist: " .. tostring(vexist) ..
+--			"\n rm_opt: " .. tostring(rm_opt) ..
+--			"\n eq_cfg: " .. tostring(eq_cfg) ..
+--			"\n eq_def: " .. tostring(eq_def) ..
+--			"\n novld : " .. tostring(novld) ..
+--			"\n errtxt: " .. tostring(errtxt) )
+	end
+
+	-- lets continue with value returned from validate
+	eq_cfg  = ( vvalue == cvalue )					-- update equal_config flag
+	local vexist = ( vvalue and (#vvalue > 0) ) and true or false	-- not "nil" and "not empty"
+	local eq_def = ( vvalue == self.default )			-- equal_default flag
+
+	-- (rmempty or optional) and (no data or equal_default)
+	if rm_opt and (not vexist or eq_def) then
+		if self:remove(section) then		-- remove data from UCI
+			self.section.changed = true	-- and push events
+		end
+		return
+	end
+
+	-- not forcewrite and no changes, so nothing to write
+	if not self.forcewrite and eq_cfg then
+		return
+	end
+
+	-- we should have a valid value here
+	assert (vvalue, "\n option: " .. self.option ..
+			"\n fvalue: " .. tostring(fvalue) ..
+			"\n fexist: " .. tostring(fexist) ..
+			"\n cvalue: " .. tostring(cvalue) ..
+			"\n vvalue: " .. tostring(vvalue) ..
+			"\n vexist: " .. tostring(vexist) ..
+			"\n rm_opt: " .. tostring(rm_opt) ..
+			"\n eq_cfg: " .. tostring(eq_cfg) ..
+			"\n eq_def: " .. tostring(eq_def) ..
+			"\n errtxt: " .. tostring(errtxt) )
+
+	-- write data to UCI; raise event only on changes
+	if self:write(section, vvalue) and not eq_cfg then
+		self.section.changed = true
+	end
+end
+
 function o.cfgvalue(self, section)
     --return fs.readfile("/SDC/Clock.conf") or ""
 	for token in string.gmatch(fs.readfile("/SDC/Clock.conf"), "[^\r\n]+") do
@@ -50,6 +146,101 @@ end
 
 o = s:option(Value, "ntpserver2", translate("Ntp Server 2"))
 o.datatype = "string"
+o.rmempty = false
+o.optional = false
+
+function o.parse(self, section, novld)
+	local fvalue = self:formvalue(section)
+	local fexist = ( fvalue and (#fvalue > 0) )	-- not "nil" and "not empty"
+	local cvalue = self:cfgvalue(section)
+	local rm_opt = ( self.rmempty or self.optional )
+	local eq_cfg					-- flag: equal cfgvalue
+
+	-- If favlue and cvalue are both tables and have the same content
+	-- make them identical
+	if type(fvalue) == "table" and type(cvalue) == "table" then
+		eq_cfg = (#fvalue == #cvalue)
+		if eq_cfg then
+			for i=1, #fvalue do
+				if cvalue[i] ~= fvalue[i] then
+					eq_cfg = false
+				end
+			end
+		end
+		if eq_cfg then
+			fvalue = cvalue
+		end
+	end
+
+	-- removed parameter "section" from function call because used/accepted nowhere
+	-- also removed call to function "transfer"
+	local vvalue, errtxt = self:validate(fvalue)
+
+	-- error handling; validate return "nil"
+	if not vvalue then
+		if novld then 		-- and "novld" set
+			return		-- then exit without raising an error
+		end
+
+		if fexist then		-- and there is a formvalue
+			self:add_error(section, "invalid", errtxt or self.title .. ": invalid")
+			return		-- so data are invalid
+		elseif not rm_opt then	-- and empty formvalue but NOT (rmempty or optional) set
+			self:add_error(section, "missing", errtxt or self.title .. ": missing")
+			return		-- so data is missing
+		elseif errtxt then
+			self:add_error(section, "invalid", errtxt)
+			return
+		end
+--		error  ("\n option: " .. self.option ..
+--			"\n fvalue: " .. tostring(fvalue) ..
+--			"\n fexist: " .. tostring(fexist) ..
+--			"\n cvalue: " .. tostring(cvalue) ..
+--			"\n vvalue: " .. tostring(vvalue) ..
+--			"\n vexist: " .. tostring(vexist) ..
+--			"\n rm_opt: " .. tostring(rm_opt) ..
+--			"\n eq_cfg: " .. tostring(eq_cfg) ..
+--			"\n eq_def: " .. tostring(eq_def) ..
+--			"\n novld : " .. tostring(novld) ..
+--			"\n errtxt: " .. tostring(errtxt) )
+	end
+
+	-- lets continue with value returned from validate
+	eq_cfg  = ( vvalue == cvalue )					-- update equal_config flag
+	local vexist = ( vvalue and (#vvalue > 0) ) and true or false	-- not "nil" and "not empty"
+	local eq_def = ( vvalue == self.default )			-- equal_default flag
+
+	-- (rmempty or optional) and (no data or equal_default)
+	if rm_opt and (not vexist or eq_def) then
+		if self:remove(section) then		-- remove data from UCI
+			self.section.changed = true	-- and push events
+		end
+		return
+	end
+
+	-- not forcewrite and no changes, so nothing to write
+	if not self.forcewrite and eq_cfg then
+		return
+	end
+
+	-- we should have a valid value here
+	assert (vvalue, "\n option: " .. self.option ..
+			"\n fvalue: " .. tostring(fvalue) ..
+			"\n fexist: " .. tostring(fexist) ..
+			"\n cvalue: " .. tostring(cvalue) ..
+			"\n vvalue: " .. tostring(vvalue) ..
+			"\n vexist: " .. tostring(vexist) ..
+			"\n rm_opt: " .. tostring(rm_opt) ..
+			"\n eq_cfg: " .. tostring(eq_cfg) ..
+			"\n eq_def: " .. tostring(eq_def) ..
+			"\n errtxt: " .. tostring(errtxt) )
+
+	-- write data to UCI; raise event only on changes
+	if self:write(section, vvalue) and not eq_cfg then
+		self.section.changed = true
+	end
+end
+
 function o.cfgvalue(self, section)
     --return fs.readfile("/SDC/Clock.conf") or ""
 	for token in string.gmatch(fs.readfile("/SDC/Clock.conf"), "[^\r\n]+") do
@@ -75,6 +266,101 @@ end
 
 o = s:option(Value, "ntpserver3", translate("Ntp Server 3"))
 o.datatype = "string"
+o.rmempty = false
+o.optional = false
+
+function o.parse(self, section, novld)
+	local fvalue = self:formvalue(section)
+	local fexist = ( fvalue and (#fvalue > 0) )	-- not "nil" and "not empty"
+	local cvalue = self:cfgvalue(section)
+	local rm_opt = ( self.rmempty or self.optional )
+	local eq_cfg					-- flag: equal cfgvalue
+
+	-- If favlue and cvalue are both tables and have the same content
+	-- make them identical
+	if type(fvalue) == "table" and type(cvalue) == "table" then
+		eq_cfg = (#fvalue == #cvalue)
+		if eq_cfg then
+			for i=1, #fvalue do
+				if cvalue[i] ~= fvalue[i] then
+					eq_cfg = false
+				end
+			end
+		end
+		if eq_cfg then
+			fvalue = cvalue
+		end
+	end
+
+	-- removed parameter "section" from function call because used/accepted nowhere
+	-- also removed call to function "transfer"
+	local vvalue, errtxt = self:validate(fvalue)
+
+	-- error handling; validate return "nil"
+	if not vvalue then
+		if novld then 		-- and "novld" set
+			return		-- then exit without raising an error
+		end
+
+		if fexist then		-- and there is a formvalue
+			self:add_error(section, "invalid", errtxt or self.title .. ": invalid")
+			return		-- so data are invalid
+		elseif not rm_opt then	-- and empty formvalue but NOT (rmempty or optional) set
+			self:add_error(section, "missing", errtxt or self.title .. ": missing")
+			return		-- so data is missing
+		elseif errtxt then
+			self:add_error(section, "invalid", errtxt)
+			return
+		end
+--		error  ("\n option: " .. self.option ..
+--			"\n fvalue: " .. tostring(fvalue) ..
+--			"\n fexist: " .. tostring(fexist) ..
+--			"\n cvalue: " .. tostring(cvalue) ..
+--			"\n vvalue: " .. tostring(vvalue) ..
+--			"\n vexist: " .. tostring(vexist) ..
+--			"\n rm_opt: " .. tostring(rm_opt) ..
+--			"\n eq_cfg: " .. tostring(eq_cfg) ..
+--			"\n eq_def: " .. tostring(eq_def) ..
+--			"\n novld : " .. tostring(novld) ..
+--			"\n errtxt: " .. tostring(errtxt) )
+	end
+
+	-- lets continue with value returned from validate
+	eq_cfg  = ( vvalue == cvalue )					-- update equal_config flag
+	local vexist = ( vvalue and (#vvalue > 0) ) and true or false	-- not "nil" and "not empty"
+	local eq_def = ( vvalue == self.default )			-- equal_default flag
+
+	-- (rmempty or optional) and (no data or equal_default)
+	if rm_opt and (not vexist or eq_def) then
+		if self:remove(section) then		-- remove data from UCI
+			self.section.changed = true	-- and push events
+		end
+		return
+	end
+
+	-- not forcewrite and no changes, so nothing to write
+	if not self.forcewrite and eq_cfg then
+		return
+	end
+
+	-- we should have a valid value here
+	assert (vvalue, "\n option: " .. self.option ..
+			"\n fvalue: " .. tostring(fvalue) ..
+			"\n fexist: " .. tostring(fexist) ..
+			"\n cvalue: " .. tostring(cvalue) ..
+			"\n vvalue: " .. tostring(vvalue) ..
+			"\n vexist: " .. tostring(vexist) ..
+			"\n rm_opt: " .. tostring(rm_opt) ..
+			"\n eq_cfg: " .. tostring(eq_cfg) ..
+			"\n eq_def: " .. tostring(eq_def) ..
+			"\n errtxt: " .. tostring(errtxt) )
+
+	-- write data to UCI; raise event only on changes
+	if self:write(section, vvalue) and not eq_cfg then
+		self.section.changed = true
+	end
+end
+
 function o.cfgvalue(self, section)
     --return fs.readfile("/SDC/Clock.conf") or ""
 	for token in string.gmatch(fs.readfile("/SDC/Clock.conf"), "[^\r\n]+") do
@@ -100,6 +386,101 @@ end
 
 o = s:option(Value, "ntpserver4", translate("Ntp Server 4"))
 o.datatype = "string"
+o.rmempty = false
+o.optional = false
+
+function o.parse(self, section, novld)
+	local fvalue = self:formvalue(section)
+	local fexist = ( fvalue and (#fvalue > 0) )	-- not "nil" and "not empty"
+	local cvalue = self:cfgvalue(section)
+	local rm_opt = ( self.rmempty or self.optional )
+	local eq_cfg					-- flag: equal cfgvalue
+
+	-- If favlue and cvalue are both tables and have the same content
+	-- make them identical
+	if type(fvalue) == "table" and type(cvalue) == "table" then
+		eq_cfg = (#fvalue == #cvalue)
+		if eq_cfg then
+			for i=1, #fvalue do
+				if cvalue[i] ~= fvalue[i] then
+					eq_cfg = false
+				end
+			end
+		end
+		if eq_cfg then
+			fvalue = cvalue
+		end
+	end
+
+	-- removed parameter "section" from function call because used/accepted nowhere
+	-- also removed call to function "transfer"
+	local vvalue, errtxt = self:validate(fvalue)
+
+	-- error handling; validate return "nil"
+	if not vvalue then
+		if novld then 		-- and "novld" set
+			return		-- then exit without raising an error
+		end
+
+		if fexist then		-- and there is a formvalue
+			self:add_error(section, "invalid", errtxt or self.title .. ": invalid")
+			return		-- so data are invalid
+		elseif not rm_opt then	-- and empty formvalue but NOT (rmempty or optional) set
+			self:add_error(section, "missing", errtxt or self.title .. ": missing")
+			return		-- so data is missing
+		elseif errtxt then
+			self:add_error(section, "invalid", errtxt)
+			return
+		end
+--		error  ("\n option: " .. self.option ..
+--			"\n fvalue: " .. tostring(fvalue) ..
+--			"\n fexist: " .. tostring(fexist) ..
+--			"\n cvalue: " .. tostring(cvalue) ..
+--			"\n vvalue: " .. tostring(vvalue) ..
+--			"\n vexist: " .. tostring(vexist) ..
+--			"\n rm_opt: " .. tostring(rm_opt) ..
+--			"\n eq_cfg: " .. tostring(eq_cfg) ..
+--			"\n eq_def: " .. tostring(eq_def) ..
+--			"\n novld : " .. tostring(novld) ..
+--			"\n errtxt: " .. tostring(errtxt) )
+	end
+
+	-- lets continue with value returned from validate
+	eq_cfg  = ( vvalue == cvalue )					-- update equal_config flag
+	local vexist = ( vvalue and (#vvalue > 0) ) and true or false	-- not "nil" and "not empty"
+	local eq_def = ( vvalue == self.default )			-- equal_default flag
+
+	-- (rmempty or optional) and (no data or equal_default)
+	if rm_opt and (not vexist or eq_def) then
+		if self:remove(section) then		-- remove data from UCI
+			self.section.changed = true	-- and push events
+		end
+		return
+	end
+
+	-- not forcewrite and no changes, so nothing to write
+	if not self.forcewrite and eq_cfg then
+		return
+	end
+
+	-- we should have a valid value here
+	assert (vvalue, "\n option: " .. self.option ..
+			"\n fvalue: " .. tostring(fvalue) ..
+			"\n fexist: " .. tostring(fexist) ..
+			"\n cvalue: " .. tostring(cvalue) ..
+			"\n vvalue: " .. tostring(vvalue) ..
+			"\n vexist: " .. tostring(vexist) ..
+			"\n rm_opt: " .. tostring(rm_opt) ..
+			"\n eq_cfg: " .. tostring(eq_cfg) ..
+			"\n eq_def: " .. tostring(eq_def) ..
+			"\n errtxt: " .. tostring(errtxt) )
+
+	-- write data to UCI; raise event only on changes
+	if self:write(section, vvalue) and not eq_cfg then
+		self.section.changed = true
+	end
+end
+
 function o.cfgvalue(self, section)
     --return fs.readfile("/SDC/Clock.conf") or ""
 	for token in string.gmatch(fs.readfile("/SDC/Clock.conf"), "[^\r\n]+") do
@@ -125,6 +506,101 @@ end
 
 o = s:option(Value, "ntpserver5", translate("Ntp Server 5"))
 o.datatype = "string"
+o.rmempty = false
+o.optional = false
+
+function o.parse(self, section, novld)
+	local fvalue = self:formvalue(section)
+	local fexist = ( fvalue and (#fvalue > 0) )	-- not "nil" and "not empty"
+	local cvalue = self:cfgvalue(section)
+	local rm_opt = ( self.rmempty or self.optional )
+	local eq_cfg					-- flag: equal cfgvalue
+
+	-- If favlue and cvalue are both tables and have the same content
+	-- make them identical
+	if type(fvalue) == "table" and type(cvalue) == "table" then
+		eq_cfg = (#fvalue == #cvalue)
+		if eq_cfg then
+			for i=1, #fvalue do
+				if cvalue[i] ~= fvalue[i] then
+					eq_cfg = false
+				end
+			end
+		end
+		if eq_cfg then
+			fvalue = cvalue
+		end
+	end
+
+	-- removed parameter "section" from function call because used/accepted nowhere
+	-- also removed call to function "transfer"
+	local vvalue, errtxt = self:validate(fvalue)
+
+	-- error handling; validate return "nil"
+	if not vvalue then
+		if novld then 		-- and "novld" set
+			return		-- then exit without raising an error
+		end
+
+		if fexist then		-- and there is a formvalue
+			self:add_error(section, "invalid", errtxt or self.title .. ": invalid")
+			return		-- so data are invalid
+		elseif not rm_opt then	-- and empty formvalue but NOT (rmempty or optional) set
+			self:add_error(section, "missing", errtxt or self.title .. ": missing")
+			return		-- so data is missing
+		elseif errtxt then
+			self:add_error(section, "invalid", errtxt)
+			return
+		end
+--		error  ("\n option: " .. self.option ..
+--			"\n fvalue: " .. tostring(fvalue) ..
+--			"\n fexist: " .. tostring(fexist) ..
+--			"\n cvalue: " .. tostring(cvalue) ..
+--			"\n vvalue: " .. tostring(vvalue) ..
+--			"\n vexist: " .. tostring(vexist) ..
+--			"\n rm_opt: " .. tostring(rm_opt) ..
+--			"\n eq_cfg: " .. tostring(eq_cfg) ..
+--			"\n eq_def: " .. tostring(eq_def) ..
+--			"\n novld : " .. tostring(novld) ..
+--			"\n errtxt: " .. tostring(errtxt) )
+	end
+
+	-- lets continue with value returned from validate
+	eq_cfg  = ( vvalue == cvalue )					-- update equal_config flag
+	local vexist = ( vvalue and (#vvalue > 0) ) and true or false	-- not "nil" and "not empty"
+	local eq_def = ( vvalue == self.default )			-- equal_default flag
+
+	-- (rmempty or optional) and (no data or equal_default)
+	if rm_opt and (not vexist or eq_def) then
+		if self:remove(section) then		-- remove data from UCI
+			self.section.changed = true	-- and push events
+		end
+		return
+	end
+
+	-- not forcewrite and no changes, so nothing to write
+	if not self.forcewrite and eq_cfg then
+		return
+	end
+
+	-- we should have a valid value here
+	assert (vvalue, "\n option: " .. self.option ..
+			"\n fvalue: " .. tostring(fvalue) ..
+			"\n fexist: " .. tostring(fexist) ..
+			"\n cvalue: " .. tostring(cvalue) ..
+			"\n vvalue: " .. tostring(vvalue) ..
+			"\n vexist: " .. tostring(vexist) ..
+			"\n rm_opt: " .. tostring(rm_opt) ..
+			"\n eq_cfg: " .. tostring(eq_cfg) ..
+			"\n eq_def: " .. tostring(eq_def) ..
+			"\n errtxt: " .. tostring(errtxt) )
+
+	-- write data to UCI; raise event only on changes
+	if self:write(section, vvalue) and not eq_cfg then
+		self.section.changed = true
+	end
+end
+
 function o.cfgvalue(self, section)
     --return fs.readfile("/SDC/Clock.conf") or ""
 	for token in string.gmatch(fs.readfile("/SDC/Clock.conf"), "[^\r\n]+") do
